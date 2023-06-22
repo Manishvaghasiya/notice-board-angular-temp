8c5a85e0449edbfb23c42d5285e46386229436c6
Manish Bhupatbhai Vaghasiya <manishvaghasiya6@gmail.com>
2023-06-22 17:52:56 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/paper-dashboard-angular
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Copyright 2019 Twilio (https://www.creative-tim.com)
 * Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-angular/blob/master/LICENSE.md)
 
-* Coded by Creative Tim
+* Coded by Twilio
 
 =========================================================
 
