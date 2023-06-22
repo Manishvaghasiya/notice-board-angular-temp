ea45d83285dfd3a86a71bc422162a763c767044d
Manish Bhupatbhai Vaghasiya <manishvaghasiya6@gmail.com>
2023-06-22 18:11:24 +0530

    UI changes

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Paper Dashboard Angular - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-dashboard-angular
-* Copyright 2019 Twilio (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-angular/blob/master/LICENSE.md)
-
-* Coded by Twilio
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
